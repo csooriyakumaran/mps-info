7a5d7c18889ac6729af752d7122192f3603e5133
csooriyakumaran <c.sooriyakumaran@gmail.com>
2026-06-20 16:06:54 -0400

    added aether library for primitive types and memory arenas

--- src/main.c
+++ src/main.c
@@ -8,18 +8,21 @@
 #include "scanivalve/mps-protocol.h"
 #include "scanivalve/mps-protocol-version.h"
 
+#define AETHER_IMPLEMENTATOIN
+#include "aether/aether.h"
+
 #define MAX_TRACKED_FRAME_GAPS 1024
 
-typedef int8_t   i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
-typedef int64_t  i64;
-typedef uint8_t  u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef float    f32;
-typedef double   f64;
+// typedef int8_t   i8;
+// typedef int16_t  i16;
+// typedef int32_t  i32;
+// typedef int64_t  i64;
+// typedef uint8_t  u8;
+// typedef uint16_t u16;
+// typedef uint32_t u32;
+// typedef uint64_t u64;
+// typedef float    f32;
+// typedef double   f64;
 
 
 typedef struct {
@@ -64,6 +67,7 @@ int main(int argc, char** argv)
     {
         fprintf(stdout, "v%s\n", MPS_INFO_VERSION_STRING);
         fprintf(stdout, "  - Scanivalve Protocol Version v%s\n  - Scanivalve Firmware Version v%s\n", MPS_PROTOCOL_VERSION_STRING, MPS_FIRMWARE_VERSION_STRING );
+        fprintf(stdout, "  -              Aether Verison v%s\n", AETHER_VERSION_STRING);
         return 0;
     }
     if (argc < 2)
